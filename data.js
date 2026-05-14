1887fb7457e093267cdf25c7d77f6538f3113885
Serraslc <serraselci04@gmail.com>
2026-05-14 13:48:23 +0300

    Remove old data.js and finalize modular question pools

--- data.js
+++
@@ -1,9 +0,0 @@
-import EASY_INCIDENTS from './easy.js';
-import MEDIUM_INCIDENTS from './medium.js';
-import HARD_INCIDENTS from './hard.js';
-
-export {
-  EASY_INCIDENTS,
-  MEDIUM_INCIDENTS,
-  HARD_INCIDENTS
-};
